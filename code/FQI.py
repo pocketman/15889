340727ba758e1c36d3dda048f90b35824bcd68b5
Yohan Jo <yohanj@cs.cmu.edu>
2015-11-24 03:01:08 -0500

    FQI
    Restricted V(quiz)=0
    Added estimated time

--- code/FQI.py
+++ code/FQI.py
@@ -9,12 +9,14 @@ from load_data import *
 import sys
 import csv
 import os
+import time
 
 discount = 0.9
 num_estimators = 10  # estimators inside the random forest 
 num_iters = 1000  # num of training iterations
 num_users = 12000  # max num of users(=trajectories) (<= sys.maxint)
 target_action = "Q235"  # every trajectory should end with this action
+# target_action = "Q315"  # every trajectory should end with this action
 feat_path = "../../data/lectures/feats.csv"
 result_dir = "../results/d"+str(discount)+"-u"+str(num_users)+"-e"+str(num_estimators)+"-i"+str(num_iters)+"-t"+target_action
 approximator_path = result_dir + "/approximator/random_forest_regressor.model"  # path to save the trained approximator
@@ -37,9 +39,12 @@ s0 = np.zeros((1,num_feats))
 approximator = RandomForest(num_estimators=num_estimators, num_actions=num_actions)
 approximator.train(cur_states, actions, rewards)
 for iter in range(num_iters):
-    print "Iteration", iter
+    print "---------------------------------------------------------------\nIteration", iter
+    start_time = time.time()
+    
     qs = approximator.predict(next_states)  # dim: num_actions x num_instances
     max_qs = np.amax(qs, axis=0)  # dim: num_instances
+    max_qs[actions==action_index[target_action]] = 0  # restrict V(quiz)=0
     approximator.train(cur_states, actions, rewards + discount * max_qs)
     
     if debug_action_cnt:
@@ -50,6 +55,9 @@ for iter in range(num_iters):
     if debug_q0:
         q0 = approximator.predict(s0)
         print "max q0:", np.amax(q0, axis=0)[0], ", max action:", action_list[np.argmax(q0, axis=0)]
+
+    print "Estimated time: ", int((time.time()-start_time)/60*(num_iters-iter-1)), "mins"
+
 
 # print
 if not os.path.exists(result_dir): 
