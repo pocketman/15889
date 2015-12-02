87ce88ab60c61e1cd218d6e8c65a2df09913cea8
Yohan Jo <yohanj@cs.cmu.edu>
2015-12-02 16:28:15 -0500

    FQI
    Modified FQI to handle demographic info.

--- code/FQI.py
+++ code/FQI.py
@@ -16,18 +16,18 @@ num_estimators = 10  # estimators inside the random forest
 num_iters = 1000  # num of training iterations
 num_users = sys.maxint  # max num of users(=trajectories) (use sys.maxint for unlimited case)
 num_users_ratio = 0.7  # % of users(=trajectories) for training
-# target_action = "Q235"  # every trajectory should end with this action
-target_action = "Q315"  # every trajectory should end with this action
-feat_path = "../../data/lectures/feats.csv"
-result_dir = "../results/d"+str(discount)+("-un"+str(num_users) if num_users != sys.maxint else "")+("-ur"+str(num_users_ratio) if num_users_ratio != 1 else "")+"-e"+str(num_estimators)+"-i"+str(num_iters)+"-t"+target_action
+demography = False
+target_action = "Q235"  # every trajectory should end with this action
+# target_action = "Q315"  # every trajectory should end with this action
+feat_path = "../../data/lectures+demography/feats.csv"
+result_dir = "../results/demo"+("1" if demography else '0')+"-d"+str(discount)+("-un"+str(num_users) if num_users != sys.maxint else "")+("-ur"+str(num_users_ratio) if num_users_ratio != 1 else "")+"-e"+str(num_estimators)+"-i"+str(num_iters)+"-t"+target_action
 approximator_path = result_dir + "/approximator/random_forest_regressor.model"  # path to save the trained approximator
 debug_action_cnt = False
 debug_q0 = True
 
 
 # Load data
-# cur_states, actions, rewards, next_states, action_index, valid_users, valid_feats = load_data(feat_path, target_action, num_users=num_users, exclude_users=valid_users, valid_feats=valid_feats, valid_actions=action_list)
-cur_states, actions, rewards, next_states, users, action_index, user_index, valid_feats = load_data(feat_path, target_action, num_users=num_users, num_users_ratio=num_users_ratio)
+cur_states, actions, rewards, next_states, users, action_index, user_index, valid_feats = load_data(feat_path, target_action, num_users=num_users, num_users_ratio=num_users_ratio, demography=demography)
 print cur_states.shape, next_states.shape, actions.shape, rewards.shape, users.shape, len(action_index), len(user_index), sum(valid_feats)
 # dim: cur_states,next_states = num_instances x num_features 
 # dim: actions,rewards = num_instances
@@ -44,12 +44,12 @@ approximator.train(cur_states, actions, rewards)
 for iter in range(num_iters):
     print "---------------------------------------------------------------\nIteration", iter
     start_time = time.time()
-     
+    
     qs = approximator.predict(next_states)  # dim: num_actions x num_instances
     max_qs = np.amax(qs, axis=0)  # dim: num_instances
-    max_qs[actions==action_index[target_action]] = 0  # restrict V(quiz)=0
+    max_qs[actions==action_index[target_action]] = 0
     approximator.train(cur_states, actions, rewards + discount * max_qs)
-     
+    
     if debug_action_cnt:
         max_as = np.argmax(qs, axis=0)
         action_cnt = defaultdict(int)
